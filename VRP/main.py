883d665f28acb2bba762cb5157d7d67562f35396
Tomasz Godzik <tomek.godzik@gmail.com>
2013-11-15 22:57:44 +0100

    Added mutation and started crossover. Example run is commented out.

--- VRP/main.py
+++ VRP/main.py
@@ -19,17 +19,18 @@ creator.create("Individual", list, fitness=creator.FitnessSolution)
 #create list of all customers
 customer=range(1,customers_num+1)
 
+
 #creating new individuals
 toolbox = base.Toolbox()
 toolbox.register("attr", randomize_list,customer,problem.vehicles)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-toolbox.register("evaluate", evaluate)
+toolbox.register("evaluate", evaluate, problem)
 #cross - ind1, ind2
 #toolbox.register("mate", tools.cxTwoPoints)
 # mutate with one ind
-#toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("mutate", mutate, problem.vehicles)
 # ind, k <- NGSA, SPEA
 #toolbox.register("select", tools.selTournament, tournsize=3)
 
@@ -37,10 +38,47 @@ population = toolbox.population(n=300)
 
 NGEN=40
 
-#ind1.fitness.values = evaluate(ind1,problem)
-#print ind1.fitness.valid
-#print ind1.fitness
+ind1 = toolbox.individual()
+
+ind1.fitness.values = evaluate(ind1,problem)
+print ind1.fitness.valid
+print ind1.fitness
+print ind1
+ind2, = mutate(ind1,problem.vehicles)
+ind2.fitness.values = evaluate(ind2,problem)
+print ind2.fitness.valid
+print ind2.fitness
+print ind2
+
 #
 #ind2.fitness.values = evaluate(ind2,problem)
 #print ind2.fitness.valid
 #print ind2.fitness
+
+#for g in range(NGEN):
+#    # Select the next generation individuals
+#    offspring = toolbox.select(pop, len(pop))
+#    # Clone the selected individuals
+#    offspring = map(toolbox.clone, offspring)
+#
+#    # Apply crossover on the offspring
+#    for child1, child2 in zip(offspring[::2], offspring[1::2]):
+#        if random.random() < CXPB:
+#            toolbox.mate(child1, child2)
+#            del child1.fitness.values
+#            del child2.fitness.values
+#
+#    # Apply mutation on the offspring
+#    for mutant in offspring:
+#        if random.random() < MUTPB:
+#            toolbox.mutate(mutant)
+#            del mutant.fitness.values
+#
+#    # Evaluate the individuals with an invalid fitness
+#    invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+#    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+#    for ind, fit in zip(invalid_ind, fitnesses):
+#        ind.fitness.values = fit
+#
+#    # The population is entirely replaced by the offspring
+#    pop[:] = offspring
--- VRP/utils/evolution.py
+++ VRP/utils/evolution.py
@@ -1,47 +1,128 @@
 __author__ = 'Tomasz Godzik'
 
 import math
+import random
 
 #what with depo?
-def evaluate_route(route,problem):
-    time=0
-    additional=0
-    current_cargo=problem.capacity
-    current_x=problem.depotx
-    current_y=problem.depoty
+def evaluate_route(route, problem):
+    time = 0
+    additional = 0
+    current_cargo = problem.capacity
+    current_x = problem.depotx
+    current_y = problem.depoty
     for i in route:
         #check if we have enough cargo (check if possible - multiple goes?)
         if current_cargo > problem.customers[i].demand:
             #calculate the distance from one customer to another
-            time+=math.sqrt(math.pow(problem.customers[i].x-current_x,2)+math.pow(problem.customers[i].y-current_y,2))
+            time += math.sqrt(
+                math.pow(problem.customers[i].x - current_x, 2) + math.pow(problem.customers[i].y - current_y, 2))
         else:
             # cost of going back to customer
-            time+=math.sqrt(math.pow(problem.depotx-current_x,2)+math.pow(problem.depoty-current_y,2))
-            time+=math.sqrt(math.pow(problem.customers[i].x-problem.depotx,2)+math.pow(problem.customers[i].y-problem.depoty,2))
-            current_cargo=problem.capacity
+            time += math.sqrt(math.pow(problem.depotx - current_x, 2) + math.pow(problem.depoty - current_y, 2))
+            time += math.sqrt(
+                math.pow(problem.customers[i].x - problem.depotx, 2) + math.pow(problem.customers[i].y - problem.depoty,
+                    2))
+            current_cargo = problem.capacity
 
         #drop cargo
-        current_cargo-=problem.customers[i].demand
+        current_cargo -= problem.customers[i].demand
 
         #change current location
-        current_x=problem.customers[i].x
-        current_y=problem.customers[i].y
+        current_x = problem.customers[i].x
+        current_y = problem.customers[i].y
 
         #if we have to wait
-        time=max(time<problem.customers[i].ready, time)
+        time = max(time < problem.customers[i].ready, time)
 
         #if we are late, add penalty
-        additional += max(0, (time - problem.customers[i].due)*10 )
+        additional += max(0, (time - problem.customers[i].due) * 10)
 
         #time it takes to drop it
-        time+=problem.customers[i].service
+        time += problem.customers[i].service
 
-    return time+additional
+    return time + additional
 
 #wyliczamy wartosc fitness
-def evaluate(individual,problem):
-    a =  len(individual)
-    b=0
+def evaluate(individual, problem):
+    a = len(individual)
+    b = 0
     for i in individual:
-        b+=evaluate_route(i,problem)
+        b += evaluate_route(i, problem)
     return a, b
+
+
+def mutate_swap(ind):
+    route1 = random.randint(0, len(ind) - 1)
+    route2 = random.randint(0, len(ind) - 1)
+
+    client1 = random.randint(0, len(ind[route1]) - 1)
+    client2 = random.randint(0, len(ind[route2]) - 1)
+
+    tmp = ind[route1][client1]
+    ind[route1][client1] = ind[route2][client2]
+    ind[route2][client2] = tmp
+
+    return ind,
+
+
+def mutate_inverse(ind):
+    which = random.randint(0, len(ind) - 1)
+    ind[which] = ind[which][::-1]
+    return ind,
+
+
+def mutate_insert(ind, max_vehicles):
+    route = random.randint(0, len(ind) - 1)
+    client = random.randint(0, len(ind[route]) - 1)
+    found = ind[route][client]
+
+    if random.random() < (1.0 / (2.0 * len(ind))) and len(ind) < max_vehicles:
+        ind.append([found])
+    else:
+        new_route = random.randint(0, len(ind) - 1)
+        place = random.randint(0, len(ind[new_route]))
+        ind[new_route].insert(place, found)
+
+    ind[route].remove(found)
+
+    if len(ind[route]) == 0:
+        ind.remove(ind[route])
+
+    return ind,
+
+
+def mutate_displace(ind,max_vehicles):
+    route = random.randint(0, len(ind) - 1)
+    rfrom = random.randint(0, len(ind[route]) - 1)
+    rto = random.randint(rfrom + 1, len(ind[route]))
+    subroute = ind[route][rfrom:rto]
+    ind[route] = ind[route][0:rfrom] + (ind[route][rto:len(ind[route])])
+
+    if random.random() < (1.0 / (2.0 * len(ind))) and len(ind) < max_vehicles:
+        ind.append(subroute)
+    else:
+        new_route = random.randint(0, len(ind) - 1)
+        place = random.randint(0, len(ind[new_route]))
+        ind[new_route]=ind[new_route][0:place] + subroute + ind[new_route][place:len(ind[new_route])]
+
+    if len(ind[route]) == 0:
+        ind.remove(ind[route])
+    return ind,
+
+def mutate(ind,max_v,swap_rate=0.05,inverse_rate=0.1,insert_rate=0.05,displace_rate=0.15):
+    if random.random() <= swap_rate:
+        ind,= mutate_swap(ind)
+    if random.random() <= inverse_rate:
+        ind,= mutate_inverse(ind)
+    if random.random() <= insert_rate:
+        ind, = mutate_insert(ind,max_v)
+    if random.random() <= displace_rate:
+        ind, = mutate_displace(ind,max_v)
+    return ind,
+
+def cross_over(ind1,ind2):
+    route = random.randint(0, len(ind1) - 1)
+    rfrom = random.randint(0, len(ind1[route]) - 1)
+    rto = random.randint(rfrom + 1, len(ind1[route]))
+    subroute = ind1[route][rfrom:rto]
+    # insert into the second one
